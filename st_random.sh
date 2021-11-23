213c1b482c3a85acdb7a667e5452cf371f843988
Evgeniy <dzaba67@yandex.ru>
2021-11-23 15:57:57 +0300

    helper: refactor script for print debugging

--- st_random.sh
+++ st_random.sh
@@ -1,10 +1,10 @@
-N=25;
 M=1000;
-
+CUR=10;
+NUM=10;
 cd build-debug || exit;
 make;
 cd ..
-for (( i = 6; i < 8; i++ )); do
-  build-debug/main $i $M
+for (( i = $CUR; i < $CUR + $NUM; i++ )); do
+  build-debug/opt_pair_tree $i $M
   pipenv run python drawgraph.py
 done
